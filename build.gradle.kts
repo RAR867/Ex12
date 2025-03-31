4d661a64d16df7c3f5c7c8de225e03761c35c7b1
Raul <arevaloraul31@gmail.com>
2025-03-31 18:08:46 -0400

    try again

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("java")
+    id("jacoco")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation ("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // Ensure tests run before generating the report
+    reports {
+        xml.required.set(true)  // Enable XML report
+        html.required.set(true) // Enable HTML report
+    }
+}
